ba2dc8f990c180821464f7c5e34248107db1be18
Alex Baldwin <alexbaldwin2876@outlook.com>
2018-04-05 15:30:52 +0800

    Fixed the MOST IMPORTANT COMMAND

--- commands/fun/cowsay.js
+++ commands/fun/cowsay.js
@@ -3,8 +3,8 @@ const { Command } = require("../../Nitro");
 class CowsayCommand extends Command {
     async run({ message, bot, reply, t }) {
         const [input] = message.args;
-        const cow = "        \\   ^__^\n         \\  (oo)\_______\n            (__)\       )\\/\\\n                ||----w |\n                ||     ||\n"
-        reply("`\n  " + '_'.repeat(input.length + 3) + "\n< " + input + " >\n " + '_'.repeat(input.length + 3) + "\n" + cow + "`");
+        const cow = "        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||\n"
+        reply("`\n." + '_'.repeat(input.length + 2) + "\n< " + input + " >\n " + '-'.repeat(input.length + 2) + "\n" + cow + "`");
     }
 
     help = "Cowsay";
