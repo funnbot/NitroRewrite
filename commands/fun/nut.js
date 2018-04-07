eda3c3478f244d6af27c283444ddf49f12b38b76
Alex Baldwin <alexbaldwin2876@outlook.com>
2018-04-08 01:01:17 +0800

    The Nut Button Meme

--- commands/fun/nut.js
+++ commands/fun/nut.js
@@ -1,35 +1,35 @@
 const { Command, Image } = require("../../Nitro");
 
-class TheSearchCommand extends Command {
+class NutCommand extends Command {
     async run({ message, bot, reply, t }) {
         const [input] = message.args;
-
-        const img = new Image.Canvas(700, 612);
-        await img.drawImage("thesearch", 0, 0, 700, 612);
-        img.ctx.font = "18px Comic Sans";
+        //?nut When he hitting it from behind and resolves your promise
+        const img = new Image.Canvas(640, 640);
+        await img.drawImage("nutmeme", 0, 0, 640, 640);
+        img.ctx.font = "42px Arial";
         var inputWords = input.split(" ");
         var inputLines = [];
         var currentInput = "";
         for (var i=0;i<inputWords.length;i++) {
             currentInput = currentInput + inputWords[i] + " ";
-            if (currentInput.length > 10) {
+            if (currentInput.length > 20) {
                 inputLines.push(currentInput);
                 currentInput = "";
             }
         }
         inputLines.push(currentInput);
-        img.ctx.fillText(inputLines.join("\n"), 65, 350);
+        img.ctx.fillText(inputLines.join("\n"), 25, 50);
 
         reply(img.send());
     }
 
-    help = "The search continues meme";
+    help = "The nut button meme";
     cooldown = 20;
     arg = {
         type: "string",
-        info: "A stupid idea",
-        example: "Indexes should start at 1"
+        info: "Text to put in the meme",
+        example: "When he hitting it from behind and resolves your promise"
     }
 }
 
-module.exports = TheSearchCommand;
+module.exports = NutCommand;
