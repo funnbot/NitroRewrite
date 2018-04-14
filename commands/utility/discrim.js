fd067638e84670459950bc015a1e120306ce1e44
funnbot <dillonbayless1@gmail.com>
2018-04-14 10:42:06 -0700

    Look at it

--- commands/utility/discrim.js
+++ commands/utility/discrim.js
@@ -1,31 +1,37 @@
-const { Command } = require("../../Nitro");
+const { Command, util: { escapeMarkdown } } = require("../../Nitro");
 
 class DiscrimCommand extends Command {
     async run({ message, bot, reply, t }) {
         const [input] = message.args;
         const disc = input.replace(/[^0123456789]/g, "");
         if (disc.length !== 4) {
-            return send("Discriminators are 4 numbers");
+            return reply.warn("Discriminators are 4 numbers");
         }
+        const req = `this.users.filter(u => u.discriminator === "${disc}").map(u => u.username)`
+        const res = await bot.shard.broadcastEval(req);
 
-        bot.shard.broadcastEval("this.users.filter(u => u.discriminator === '"+disc+"').map(u => u.username)").then(ret => {
-            var i=0;
-            for (i=0;i<ret.length-1;i++) {
-                ret[0].concat(ret[i+1])
+        const users = [].concat(...res);
+        const txt = `**Users with the discriminator: ${disc}**\n${escapeMarkdown(users.join(", "))}`;
+        return reply(txt.shorten());
+        /*bot.shard.broadcastEval(req).then(ret => {
+            var i = 0;
+            for (i = 0; i < ret.length - 1; i++) {
+                ret[0].concat(ret[i + 1])
             }
             var filt = {}
-            for (i=0;i<ret[0].length;i++) {
+            for (i = 0; i < ret[0].length; i++) {
                 filt[ret[0][i]] = true
             }
             filt = Object.keys(filt)
-            reply("**Users with the Discriminator: "+disc+"**\n"+filt.join(", "))
-        })
+            reply("**Users with the Discriminator: " + disc + "**\n" + filt.join(", "))
+        })*/
     }
 
-    help = "Search for a user by their discriminant";
+    help = "Search for a user by their discriminator";
+    cooldown = 5;
     arg = {
         type: "string",
-        info: "Discriminant",
+        info: "Discriminator",
         example: "5645",
     }
 }
