c41fdcdbb45c1163fb011032de72659181a60503
funnbot <dillonbayless1@gmail.com>
2018-04-29 22:40:07 -0700

    discrm coments

--- commands/utility/discrim.js
+++ commands/utility/discrim.js
@@ -13,18 +13,6 @@ class DiscrimCommand extends Command {
         const users = [].concat(...res);
         const txt = `**Users with the discriminator: ${disc}**\n${escapeMarkdown(users.join(", "))}`;
         return reply(txt.shorten());
-        /*bot.shard.broadcastEval(req).then(ret => {
-            var i = 0;
-            for (i = 0; i < ret.length - 1; i++) {
-                ret[0].concat(ret[i + 1])
-            }
-            var filt = {}
-            for (i = 0; i < ret[0].length; i++) {
-                filt[ret[0][i]] = true
-            }
-            filt = Object.keys(filt)
-            reply("**Users with the Discriminator: " + disc + "**\n" + filt.join(", "))
-        })*/
     }
 
     help = "Search for a user by their discriminator";
